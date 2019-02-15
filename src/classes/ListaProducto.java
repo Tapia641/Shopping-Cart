0236588296fbfe75804cac894477a5c597059657
tapia <tapia641@gmail.com>
2019-02-14 20:38:55 -0600

    Creando Producto

---
+++ src/classes/ListaProducto.java
@@ -0,0 +1,28 @@
+package classes;
+
+import java.util.Stack;
+import java.util.Vector;
+
+public class ListaProducto {
+    private Vector<Producto> Lista;
+
+    public ListaProducto() {
+        Lista = new Stack<>();
+    }
+
+    public Vector<Producto> getLista() {
+        return Lista;
+    }
+
+    public void setLista(Vector<Producto> lista) {
+        Lista = lista;
+    }
+
+    public void pushProducto(Producto p) {
+        Lista.add(p);
+    }
+
+    public int getTamLista(){
+        return Lista.size();
+    }
+}
--- src/classes/Producto.java
+++ src/classes/Producto.java
@@ -1,4 +1,42 @@
 package classes;
 
+import java.awt.*;
+
 public class Producto {
+    private String NombreProducto;
+    private String Descripcion;
+    private Image ImagenMuestra;
+
+    public Producto() {
+    }
+
+    public Producto(String nombreProducto, String descripcion, Image imagenMuestra) {
+        NombreProducto = nombreProducto;
+        Descripcion = descripcion;
+        ImagenMuestra = imagenMuestra;
+    }
+
+    public String getNombreProducto() {
+        return NombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        NombreProducto = nombreProducto;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public Image getImagenMuestra() {
+        return ImagenMuestra;
+    }
+
+    public void setImagenMuestra(Image imagenMuestra) {
+        ImagenMuestra = imagenMuestra;
+    }
 }
