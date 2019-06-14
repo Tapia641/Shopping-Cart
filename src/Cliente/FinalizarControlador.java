4b786f0e680dab5f3683792204bd860769340ed8
tapia <tapia641@gmail.com>
2019-06-13 20:19:51 -0500

    Finalizado.

--- src/Cliente/FinalizarControlador.java
+++ src/Cliente/FinalizarControlador.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -13,8 +13,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.util.*; 
-  import javax.*; 
-  import javax.activation.*; 
+import javax.*; 
+import javax.activation.*;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
